00f9a2f4182c334fd2483cd8ed878a129385b3ae
zul <iiizulfiannn@gmail.com>
2025-08-27 17:44:49 +0700

    feat: add pin data store

---
+++ app/src/main/java/com/luckyfriday/treackerexpensesapp/PinDatastore.kt
@@ -0,0 +1,27 @@
+package com.luckyfriday.treackerexpensesapp
+
+import android.content.Context
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class PinDatastore(private val context: Context) {
+    private val Context.dataStore by preferencesDataStore(name = "pin_code")
+
+    suspend fun savePin(pinCode: Int) {
+        context.dataStore.edit { preferences ->
+            preferences[PIN_CODE] = pinCode
+        }
+    }
+
+    val readPin: Flow<Int>
+        get() = context.dataStore.data.map { preferences ->
+            preferences[PIN_CODE] ?: 0
+        }
+
+    companion object {
+        private val PIN_CODE = intPreferencesKey("pin_code")
+    }
+}
--- app/src/main/java/com/luckyfriday/treackerexpensesapp/di/RepositoryModule.kt
+++ app/src/main/java/com/luckyfriday/treackerexpensesapp/di/RepositoryModule.kt
@@ -1,7 +1,8 @@
 package com.luckyfriday.treackerexpensesapp.di
 
+import com.luckyfriday.treackerexpensesapp.PinDatastore
 import org.koin.dsl.module
 
 val repositoryModule = module {
-
+    single { PinDatastore(context = get()) }
 }
